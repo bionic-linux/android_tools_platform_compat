8b38c6beabd149957454648a8825e1fa94946e84
Paul Duffin <paulduffin@google.com>
2021-04-14 17:21:26 +0100

    Add preupload script to prevent additions to hidden API flag files
    To use just add the following to the [Hook Scripts] section of the
    PREUPLOAD.cfg file:
    
    hidden_api_txt_prevent_additions_hook = ${REPO_ROOT}/tools/platform-compat/hiddenapi/prevent_additions.sh ${PREUPLOAD_COMMIT} ${REPO_ROOT}
    
    Bug: 177892522
    Test: attempt to upload changes with various changes to hidden API flag files
    Change-Id: Ifa2f4fffa265e89c171c1263797f55efc379969a

---
+++ hiddenapi/prevent_additions.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+set -e
+# Make sure that additional entries are not added to any of the hiddenapi files.
+LOCAL_DIR="$( dirname ${BASH_SOURCE} )"
+
+SHA=$1
+
+for file in $(git show --name-only --pretty=format: $SHA | grep "hiddenapi/hiddenapi-.*txt"); do
+    ENTRIES=$(grep -E "^\+[^+]" <(git diff ${SHA}~1 ${SHA} $file) | sed "s|^\+||" || echo)
+    if [[ -n "${ENTRIES}" ]]; then
+      echo -e "\e[1m\e[31m$file $SHA contains the following entries\e[0m"
+      echo -e "\e[1m\e[31mPlease remove these entries and add UnsupportedAppUsage annotations instead.\e[0m"
+      for i in ${ENTRIES}
+      do
+        echo -e "\e[33m  ${i}\e[0m"
+      done
+      EXIT_CODE=1
+    fi
+done
+exit $EXIT_CODE
